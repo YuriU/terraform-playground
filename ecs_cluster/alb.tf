7e9992aad4ddad43b853ce2c981d37a2a565ddb3
YuriU <jerryhire8@gmail.com>
2019-12-26 11:01:41 +0200

    Added alb

---
+++ ecs_cluster/alb.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "web_server" {
+  
+  #inbound
+  ingress {
+	  from_port = 80
+	  to_port = 80
+	  protocol = "tcp"
+	  cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+	  from_port = 0
+	  to_port = 0
+	  protocol = "-1"
+	  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_alb" "main" {
+    name = "my-alb-ecs"
+    subnets = ["${data.aws_subnet.example.*.id}"]
+    security_groups = ["${aws_security_group.web_server.id}"]
+}
