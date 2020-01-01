d2a8306cfb7a20602df4b776a8ac47506d865cec
YuriU <jerryhire8@gmail.com>
2020-01-01 19:23:54 +0200

    Added domain name

--- cognito/main.tf
+++ cognito/main.tf
@@ -3,5 +3,10 @@ resource "aws_cognito_user_pool" "pool" {
 
   alias_attributes = []
 
-  
+
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "addsdrsderera"
+  user_pool_id = "${aws_cognito_user_pool.pool.id}"
 }
