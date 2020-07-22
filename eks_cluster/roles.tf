d829395b6725d3146efad05c79cfbbf10ba206fc
YuriU <jerryhire8@gmail.com>
2020-07-22 23:06:05 +0300

    Added eks node group role

--- eks_cluster/roles.tf
+++ eks_cluster/roles.tf
@@ -1,3 +1,4 @@
+# EKS cluster Role
 data "aws_iam_policy_document" "eks-instance-policy" {
     statement {
         actions = ["sts:AssumeRole"]
@@ -23,4 +24,39 @@ resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
 resource "aws_iam_role_policy_attachment" "eks-service-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = "${aws_iam_role.eks-cluster-role.name}"
+}
+
+
+# Node Group Role
+
+data "aws_iam_policy_document" "eks-node-group-policy" {
+    statement {
+        actions = ["sts:AssumeRole"]
+
+        principals {
+            type = "Service"
+            identifiers = ["ec2.amazonaws.com"]
+        }
+    }
+}
+
+resource "aws_iam_role" "eks-node-group-role" {
+    name = "eks-node-group-role"
+    path = "/"
+    assume_role_policy = "${data.aws_iam_policy_document.eks-node-group-policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = "${aws_iam_role.eks-node-group-role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = "${aws_iam_role.eks-node-group-role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = "${aws_iam_role.eks-node-group-role.name}"
 }
