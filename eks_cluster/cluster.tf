9a203f7ba5f80345e8dc2a249125e16d988e0902
YuriU <jerryhire8@gmail.com>
2020-07-23 00:02:34 +0300

    Added node groups

--- eks_cluster/cluster.tf
+++ eks_cluster/cluster.tf
@@ -11,8 +11,8 @@ data "aws_subnet" "default_subnets" {
   id    = "${data.aws_subnet_ids.default_subnets_ids.ids[count.index]}"
 }
 
-resource "aws_eks_cluster" "example" {
-  name     = "example"
+resource "aws_eks_cluster" "cluster" {
+  name     = "cluster"
   role_arn = "${aws_iam_role.eks-cluster-role.arn}"
 
   vpc_config {
@@ -28,9 +28,9 @@ resource "aws_eks_cluster" "example" {
 }
 
 output "endpoint" {
-  value = "${aws_eks_cluster.example.endpoint}"
+  value = "${aws_eks_cluster.cluster.endpoint}"
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = "${aws_eks_cluster.example.certificate_authority.0.data}"
+  value = "${aws_eks_cluster.cluster.certificate_authority.0.data}"
 }
---
+++ eks_cluster/node_group.tf
@@ -0,0 +1,21 @@
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = "${aws_eks_cluster.cluster.name}"
+  node_group_name = "node_group"
+  node_role_arn   = "${aws_iam_role.eks-node-group-role.arn}"
+  subnet_ids      = ["${data.aws_subnet.default_subnets.*.id}"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  /*depends_on = [
+    "${aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy}",
+    "${aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy}",
+    "${aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly}",
+    
+  ]*/
+}
